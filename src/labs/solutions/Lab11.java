d8b2cbdc12935946c7bc1daa7e7505a7acf473cf
Michael <michael.oneill.2020@mumail.ie>
2022-01-02 14:40:02 +0000

    Fix lab 11 error

--- src/labs/solutions/Lab11.java
+++ src/labs/solutions/Lab11.java
@@ -6,7 +6,6 @@ import java.util.Scanner;
 
 public class Lab11 {
     public static void main(String[] args) {
-        // read input
         Scanner scan = new Scanner(System.in);
         String[] input = new String[scan.nextInt()]; scan.nextLine();
         String[] hash = new String[input.length];
@@ -16,21 +15,18 @@ public class Lab11 {
         }
         scan.close();
 
-        // find pair of hashes with least difference and print first one
         int index1 = -1;
-        int index2 = -1;
         int maxSim = Integer.MIN_VALUE;
         for(int i=0; i < input.length -1; i++) {
             for(int j=i+1; j < input.length; j++) {
                 int sim = similarity(hash[i], hash[j]);
                 if(sim > maxSim) {
                     index1 = i;
-                    index2 = j;
                     maxSim = sim;
                 }
             }
         }
-        System.out.println(hash[index1]);
+        System.out.println(input[index1]);
     }
 
     public static int similarity(String a, String b) {
--- src/practice/recursion/MergeSortLinkedList.java
+++ src/practice/recursion/MergeSortLinkedList.java
@@ -1,16 +1,18 @@
 package practice.recursion;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class MergeSortLinkedList {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Node head = new Node("");
+        File input = new File("src/practice/recursion/words/verbs");
         try {
-            Scanner scan = new Scanner(new File("src/resources/words"));
+            Scanner scan = new Scanner(input);
             head.value = scan.nextLine();
             Node current = head;
-            int scanned = 0;
             while(scan.hasNext()) {
                 Node nextNode = new Node(scan.nextLine());
                 current.next = nextNode;
@@ -20,10 +22,27 @@ public class MergeSortLinkedList {
         catch(Exception e) {
             e.printStackTrace();
         }
+        System.out.println("Original:\n"+head+"\n");
 
-        System.out.println(
-                Sorting.mergeSort(head, Sorting.Order.ASCENDING)
-        );
+        Sorting.mergeSort(head);
+        String outputName = input.getCanonicalPath()+"_sorted.txt";
+        System.out.println("Writing to: "+outputName+"...");
+        File output = new File(outputName);
+        output.createNewFile();
+        FileWriter file = new FileWriter(output);
+
+        Node current = head;
+        boolean first=true;
+        while(current != null) {
+            System.out.println(current.value);
+            if(first) {
+                file.append(current.value);
+                first=false;
+            }
+            else file.append("\n"+current.value);
+            current = current.next;
+        }
+        file.close();
     }
 }
 
@@ -57,20 +76,14 @@ class Node {
             sb.append(String.format("%s\n", current.value));
             current = current.next;
         }
-        return sb.toString();
+        String sbString = sb.toString();
+        return sbString.substring(0, sbString.length()-1);
     }
 }
 
 class Sorting {
-    enum Order {
-        ASCENDING, DESCENDING;
-    }
 
     public static Node mergeSort(Node head) {
-        return mergeSort(head, Order.ASCENDING);
-    }
-
-    public static Node mergeSort(Node head, Order ord) {
         if (head == null || !head.hasNext())
             return head;
         //Split list
@@ -80,16 +93,16 @@ class Sorting {
         right.next = null;
         right = temp;
 
-        left = mergeSort(left, ord);
-        right = mergeSort(right, ord);
-        return merge(left, right, ord);
+        left = mergeSort(left);
+        right = mergeSort(right);
+        return merge(left, right);
     }
 
-    private static Node merge(Node headLeft, Node headRight, Order ord) {
+    private static Node merge(Node headLeft, Node headRight) {
         Node tail = new Node("");
         Node dummy = tail;
         while (headLeft != null && headRight != null) {
-            if (compare(headLeft, headRight, ord)) {
+            if (compare(headLeft, headRight)) {
                 tail.next = headLeft;
                 headLeft = headLeft.next;
             } else {
@@ -103,11 +116,8 @@ class Sorting {
         return dummy.next;
     }
 
-    private static boolean compare(Node a, Node b, Order ord) {
+    private static boolean compare(Node a, Node b) {
         //Compares Node values alphabetically
-        if (ord == Order.ASCENDING) {
-            return a.value.compareTo(b.value) < 0;
-        }
-        return a.value.compareTo(b.value) > 0; //DESCENDING
+        return a.value.compareTo(b.value) < 0;
     }
 }
